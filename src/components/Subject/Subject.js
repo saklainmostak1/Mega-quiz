d42bff20f3cd8c2d4919dd816ff02f33b7e3597e
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-11 21:01:37 +0600

    icon card

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -3,8 +3,7 @@ import './Subject.css'
 import { EyeIcon } from '@heroicons/react/24/solid'
 
 
-const Subject = ({quiz}) => {
-     
+const Subject = ({quiz}) => {  
     const {correctAnswer, question, options} = quiz
     const correctAnsss = correctAnswer
         const correctAns = () =>{
@@ -12,30 +11,24 @@ const Subject = ({quiz}) => {
                     alert(correctAnsss)
             }
         }
-    return (
-        <div className='container p-3'>
-            
-          <div className='bg-light rounded text-primary shadow-lg p-5'>
-            <div className='d-flex '>
-            <h5>Question: {question} </h5>
-            <button onClick={correctAns} className='bg-light border border-0'><EyeIcon className="h-6 w-6  w-25 h-25"/></button>
-            </div>
-           
-            <div className='btn d-flex justify-content-center'>
-                <div className=''>
-                    <p><button className='btn btn-primary text-dark'  >{options[0]}</button></p>
-                    <p><button  className='btn btn-primary text-dark'>{options[1]}</button></p>
-                </div>
-                <div className='px-5'>
-                <p><button  className='btn btn-primary text-dark' >{options[2]}</button></p>
-                <p><button  className='btn btn-primary text-dark'>{options[3]}</button></p>
-               
-              
-                </div>
-            </div> 
+    return (  
+       <div class="card text-center container m-5">
+           <div class="card-body">
+                <div className='d-flex'>
+                    <h5 class="card-title"> {question} </h5>
+                     <p onClick={correctAns} className='bg-white border border-0'><EyeIcon className="h-6 w-6  w-25 h-25"/></p>
+        </div>
+             <div className='d-grid btn'>
+                 <button  class="btn btn-info  p-4 m-2">{options[0]}</button>
+                 <button  class="btn btn-info  p-4 m-2">{options[1]}</button>
+                 <button  class="btn btn-info  p-4 m-2">{options[2]}</button>
+                <button  class="btn btn-info  p-4 m-2">{options[3]}</button>
 
+             </div>
         </div>
-        </div>
+    </div>
+
+        
     );
 };
 
--- src/components/Toastify/Toast.js
+++ src/components/Toastify/Toast.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 const Toast = () => {
-        const notify = () => toast("correct ans");
+    const notify = () => toast("correct ans");
     return (
         <div>
-             <div class="d-grid gap-2">
+            <div class="d-grid gap-2">
                 <button onClick={notify} class="btn btn-primary" type="button"></button>
         <ToastContainer />
       </div>
