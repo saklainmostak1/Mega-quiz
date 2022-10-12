e1c5f65e746218e5fafae4233fafe6ebc244d7d5
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-12 21:31:57 +0600

    push

--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
 
 const TopicDetails = ({topic}) => {
     const {logo, name, id, total} = topic
@@ -13,7 +14,7 @@ const TopicDetails = ({topic}) => {
       <p>Total Qus: {total} </p>
       <div className="card-body  d-flex justify-content-between">
         <h5 className="card-title">{name}</h5>
-        <Link to={`/quiz/${id}`} ><button className='btn btn-primary'>Start Practice</button></Link>
+        <Link to={`/quiz/${id}`} ><button className='btn btn-primary d-flex'>Start Practice  <ArrowRightIcon className=" text-black "/></button></Link>
       </div>
     </div>
   </div>
