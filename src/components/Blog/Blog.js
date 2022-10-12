66e4e24732a0e9f493f2c344bbdc20f0d20e204f
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-12 21:02:10 +0600

    push

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -10,7 +10,6 @@ const Blog = () => {
     <h5 className="card-title text-decoration-underline"><strong>Purpose of react router:</strong></h5>
     <p className="card-text"> 
                     ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
-   
         </div>
          </div>
          </section>
@@ -21,7 +20,6 @@ const Blog = () => {
     <h5 className="card-title text-decoration-underline"><strong>Context api works:</strong></h5>
     <p className="card-text"> 
     The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
-   
         </div>
          </div>
             </section>
