97f162e0bdac8ef30785e7eb572c5e5a94e1b8b0
saklainMostake <saklain15-12003@diu.edu.bd>
2022-10-12 21:40:16 +0600

    all done

--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowRightIcon } from '@heroicons/react/24/solid'
 
+
 const TopicDetails = ({topic}) => {
     const {logo, name, id, total} = topic
     return (
